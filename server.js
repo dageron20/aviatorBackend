dba83cd2013184f3b3da2a359818156178cceebc
dageron20 <den-kir1@yandex.ru>
2024-06-21 01:38:48 +0300

    fix

--- server.js
+++ server.js
@@ -7,22 +7,28 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
-const db = new sqlite3.Database(':memory:');
-
-db.serialize(() => {
-  db.run("CREATE TABLE users (login TEXT, password TEXT)");
-  db.run("CREATE TABLE data (field1 INTEGER, field2 INTEGER)");
-
-  const stmt = db.prepare("INSERT INTO users VALUES (?, ?)");
-  stmt.run("admin", "password");  // Логин и пароль для входа
-  stmt.finalize();
-
-  db.run("INSERT INTO data (field1, field2) VALUES (0, 0)"); // Начальные значения полей
+const db = new sqlite3.Database('./aviator.db', (err) => {
+    if (err) {
+        console.error('Could not connect to database', err);
+    } else {
+        console.log('Connected to the SQLite database');
+    }
 });
+
+// db.serialize(() => {
+//   db.run("CREATE TABLE users (login TEXT, password TEXT)");
+//   db.run("CREATE TABLE data (field1 INTEGER, field2 INTEGER)");
+
+//   const stmt = db.prepare("INSERT INTO users VALUES (?, ?)");
+//   stmt.run("admin", "password");  // Логин и пароль для входа
+//   stmt.finalize();
+
+//   db.run("INSERT INTO data (field1, field2) VALUES (0, 0)"); // Начальные значения полей
+// });
 
 app.post('/api/login', (req, res) => {
   const { login, password } = req.body;
-  db.get("SELECT * FROM users WHERE login = ? AND password = ?", [login, password], (err, row) => {
+  db.get("SELECT * FROM Accounts WHERE login = ? AND password = ?", [login, password], (err, row) => {
     if (row) {
       res.json({ success: true });
     } else {
