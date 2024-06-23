b74685829c20b3732054aec3828c5301f8cd69a4
dageron20 <den-kir1@yandex.ru>
2024-06-23 16:24:26 +0300

    cors fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: 'http://147.45.185.47:3000',
+  origin: 'http://techdomaintest.tw1.ru/',
 }));
 
 const db = new sqlite3.Database('./aviator.db', (err) => {
