7ddb4b66dc1cf002c0c03cb314099fddcb28ffcb
dageron20 <den-kir1@yandex.ru>
2024-07-04 17:01:22 +0300

    fix domen

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: 'http://147.45.185.47',
+  origin: 'http://odds96aviator.in',
 }));
 
 const db = new sqlite3.Database('./aviator.db', (err) => {
